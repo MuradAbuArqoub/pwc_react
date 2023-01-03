388dc9f3abb5e3951d074985e9ebf1e67ed7c846
MuradAbuArqoub <muradaboarkob2000@gmail.com>
2023-01-04 00:03:54 +0300

    final project

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchBox from "./SearchBox";
 import Maps from "./Maps";
 
 function App() {
+  const [selectPosition, setSelectPosition] = useState(null);
+
   return (
-   <div style={{display: "flex", flexDirection: "row", width: "100vw", height: "100vh"}}>
-    <div style={{ width: "50vw", height: "100vh" }}>
-      <Maps />
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        width: "100vw",
+        height: "100vh",
+      }}
+    >
+      <div style={{ width: "50vw", height: "100%" }}>
+        <Maps selectPosition={selectPosition} />
+      </div>
+      <div style={{ width: "50vw" }}>
+        <SearchBox selectPosition={selectPosition} setSelectPosition={setSelectPosition}/>
+      </div>
     </div>
-    <div style={{ width: "50vw" }}>
-      <SearchBox />
-    </div>
-   </div>
   );
 }
 
